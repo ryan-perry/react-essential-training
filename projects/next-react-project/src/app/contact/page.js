8c2f8dbd92745552eb6a8f78606e3bcd56f131ca
Ryan Perry <ryan@ryanperry.me>
2025-08-17 17:01:39 -0400

    Setup initial contact form layout.

--- projects/next-react-project/src/app/contact/page.js
+++ projects/next-react-project/src/app/contact/page.js
@@ -1,5 +1,44 @@
 export default function Page() {
   return (
-    <h1>Contact Us</h1>
+    <main className="max-w-md mx-auto p-6 bg-white shadow-md rounded-md">
+      <h1 className="text-2xl font-bold text-center mb-6 text-black">
+        Contact Us
+      </h1>
+      <form className="space-y-4">
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            required
+            className="focus:ring-2 focus:ring-blue-500 border-gray-300 border"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="message"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            required
+            rows={4}
+            className="focus:ring-2 focus:ring-blue-500 border-gray-300 border"
+          ></textarea>
+        </div>
+        <button type="submit" className="text-white bg-blue-600 rounded-md p-3">
+          Send message
+        </button>
+      </form>
+    </main>
   );
 }
