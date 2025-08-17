7b53f2dc9640c3143018020c33176478976c7a02
Ryan Perry <ryan@ryanperry.me>
2025-08-17 16:14:59 -0400

    Add data fetch and pass to component.

--- projects/next-react-project/src/app/hotels/page.js
+++ projects/next-react-project/src/app/hotels/page.js
@@ -1,8 +1,32 @@
-export default function Page() {
+
+async function getData() {
+  const res = await fetch('https://snowtooth-hotel-api.fly.dev');
+  return res.json();
+}
+
+
+function HotelBlock({name, capacity}) {
+  return (
+    <div className="mb-4">
+      <h2>{name}</h2>
+      <p>capacity: { capacity }</p>
+    </div>
+  );
+}
+
+
+export default async function Page() {
+  const data = await getData();
+
   return (
     <main>
       <div>
         <h1>Hotel Details</h1>
+        <div>
+          { data.map((hotel) => (
+            <HotelBlock key={hotel.id} name={hotel.name} capacity={hotel.capacity} />
+          ))}
+        </div>
       </div>
     </main>
   );
