9727f7e9459b8eeb9d2cf5bb48d55475cd20fb93
Ryan Perry <ryan@ryanperry.me>
2025-08-17 09:24:26 -0400

    Add some list items to be displayed for React Project demo.

--- projects/react-project/src/App.jsx
+++ projects/react-project/src/App.jsx
@@ -1,9 +1,34 @@
 import "./App.css";
 
+function Header({ name }) {
+  return (
+    <header>
+      <h1>{name}'s Kitchen</h1>
+    </header>
+  );
+}
+
+const items = ["Macaroni and Cheese", "Salmon with Potatoes", "Pizza"];
+
+function Main({ dishes }) {
+  return (
+    <ul>
+      {dishes.length > 0
+        ? dishes.map((dish, idx) => (
+            <li key={idx} style={{ listStyle: "none" }}>
+              {dish}
+            </li>
+          ))
+        : ""}
+    </ul>
+  );
+}
+
 function App() {
   return (
     <>
-      <h1>React Project</h1>
+      <Header name="Homer" />
+      <Main dishes={items} />
     </>
   );
 }
