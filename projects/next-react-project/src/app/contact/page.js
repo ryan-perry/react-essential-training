c7ce41e38b55dedb394fcf4fe27e21dc7f58bc96
Ryan Perry <ryan@ryanperry.me>
2025-08-17 17:10:04 -0400

    Add a simple server function to contact form.

--- projects/next-react-project/src/app/contact/page.js
+++ projects/next-react-project/src/app/contact/page.js
@@ -1,10 +1,24 @@
 export default function Page() {
+  async function submitForm(formData) {
+    "use server";
+
+    const formFields = {
+      email: formData.get("email"),
+      message: formData.get("message"),
+    };
+
+    console.log("formFields: ", formFields);
+    console.log("TODO: send these form field values to a backend");
+
+    return formFields;
+  }
+
   return (
     <main className="max-w-md mx-auto p-6 bg-white shadow-md rounded-md">
       <h1 className="text-2xl font-bold text-center mb-6 text-black">
         Contact Us
       </h1>
-      <form className="space-y-4">
+      <form className="space-y-4" action={submitForm}>
         <div>
           <label
             htmlFor="email"
@@ -17,7 +31,7 @@ export default function Page() {
             type="email"
             name="email"
             required
-            className="focus:ring-2 focus:ring-blue-500 border-gray-300 border"
+            className="focus:ring-2 focus:ring-blue-500 border-gray-300 border text-gray-700"
           />
         </div>
         <div>
@@ -32,7 +46,7 @@ export default function Page() {
             name="message"
             required
             rows={4}
-            className="focus:ring-2 focus:ring-blue-500 border-gray-300 border"
+            className="focus:ring-2 focus:ring-blue-500 border-gray-300 border text-gray-700"
           ></textarea>
         </div>
         <button type="submit" className="text-white bg-blue-600 rounded-md p-3">
