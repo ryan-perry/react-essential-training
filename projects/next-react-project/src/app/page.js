d64e8d4578b95d27a1af4de4399420e72432856b
Ryan Perry <ryan@ryanperry.me>
2025-08-17 10:44:21 -0400

    Setup Next.js demo project

---
+++ projects/next-react-project/src/app/page.js
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <main className="font-sans min-h-screen flex flex-col items-center justify-between p-24">
+      <h1>More React to Come</h1>
+    </main>
+  );
+}
